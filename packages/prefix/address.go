06a5ccc5f725b6fc41241c8faca50a14a0d80040
pdaambrosio <pda.ambrosio@gmail.com>
2023-06-28 21:02:00 -0300

    feat: package address

---
+++ packages/prefix/address.go
@@ -0,0 +1,8 @@
+package prefix
+
+var City string = "San Francisco"
+var State string = "California"
+var Country string = "USA"
+var CityCode int = 415
+var StateCode int = 650
+var CountryCode int = 1
