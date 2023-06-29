a607bc959c5e5dfda7ed9c1e83c93bfbbb5bf350
pdaambrosio <pda.ambrosio@gmail.com>
2023-06-29 17:28:14 -0300

    feat: git ignore

--- packages/prefix/address.go
+++
@@ -1,8 +0,0 @@
-package prefix
-
-var City string = "San Francisco"
-var State string = "California"
-var Country string = "USA"
-var CityCode int = 415
-var StateCode int = 650
-var CountryCode int = 1
