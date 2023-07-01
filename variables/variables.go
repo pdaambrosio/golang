f637057d04441be97e55882dc5a81157032d87f0
pdaambrosio <pda.ambrosio@gmail.com>
2023-07-01 18:12:47 -0300

    refact: variables

--- variables/variables.go
+++ variables/variables.go
@@ -2,13 +2,42 @@ package main
 
 import "fmt"
 
+// String can't to be NULL
 var address string
 var phone string = "123-456-7890"
 var name, lastname string = "John", "Doe"
-var qtd int
+
+// Boolean
 var buy bool
-var value float64
-var words rune
+
+// Integers
+var qtd int // 32 bits or 64 bits
+var qtd2 int8 // 8 bits -128 to 127
+var qtd3 int16 // 16 bits -32768 to 32767
+var qtd4 int32 // 32 bits -2147483648 to 2147483647
+var qtd5 int64 // 64 bits -9223372036854775808 to 9223372036854775807
+var qtd6 uint // 32 bits or 64 bits (unsigned)
+var qtd7 uint8 // 8 bits 0 to 255 (unsigned)
+var qtd8 uint16 // 16 bits 0 to 65535 (unsigned)
+var qtd9 uint32 // 32 bits 0 to 4294967295 (unsigned)
+var qtd10 uint64 // 64 bits 0 to 18446744073709551615 (unsigned)
+var qtd11 uintptr // big enough to hold the bit pattern of any pointer
+
+// Rune
+var words rune // alias for int32, but the value expected is a unicode character
+
+// Byte
+var value byte // alias for uint8, but the value expected is a byte
+
+// Float
+var value1 float32 // 32 bits (IEEE-754)
+var value2 float64 // 64 bits (IEEE-754)
+
+// Complex
+var value3 complex64 // 32 bits for imaginary part and an alias for float32, example: 12.8i
+var value4 complex128 // 64 bits for imaginary part and an alias for float64, example: 12.8i
+
+// By not declaring the data type, the compiler will automagically define a type for that variable
 
 var (
 	information     string
@@ -26,7 +55,7 @@ func main() {
 	fmt.Printf("lastname: %s\r\n", lastname)
 	fmt.Printf("qtd: %d\r\n", qtd)
 	fmt.Printf("buy: %t\r\n", buy)
-	fmt.Printf("value: %f\r\n", value)
+	fmt.Printf("value: %f\r\n", value1)
 	fmt.Printf("words: %c\r\n", words)
 	fmt.Printf("information: %s\r\n", information)
 	fmt.Printf("age: %d\r\n", age)
