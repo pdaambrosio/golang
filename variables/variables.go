9fe8211eb7c5d4f819dff05ef6010ff21542826e
pdaambrosio <pda.ambrosio@gmail.com>
2023-07-01 18:36:21 -0300

    feat: conversion variables

--- variables/variables.go
+++ variables/variables.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strconv"
 
 // String can't to be NULL
 var address string
@@ -47,6 +48,84 @@ var (
 )
 
 
+// Constants
+const (
+	PI = 3.14
+	Language = "Go"
+)
+
+// Constants with iota
+
+const (
+	_ = iota // 0
+	KB = 1 << (10 * iota) // 1 << (10 * 1) = 1024
+	MB = 1 << (10 * iota) // 1 << (10 * 2) = 1048576
+	GB = 1 << (10 * iota) // 1 << (10 * 3) = 1073741824
+	TB = 1 << (10 * iota) // 1 << (10 * 4) = 1099511627776
+	PB = 1 << (10 * iota) // 1 << (10 * 5) = 1125899906842624
+	EB = 1 << (10 * iota) // 1 << (10 * 6) = 1152921504606846976
+	ZB = 1 << (10 * iota) // 1 << (10 * 7) = 1180591620717411303424
+	YB = 1 << (10 * iota) // 1 << (10 * 8) = 1208925819614629174706176
+)
+
+// iota is a predeclared identifier representing the untyped integer ordinal number of the current const specification in a (usually parenthesized) const declaration. It is zero-indexed.
+// Within a constant declaration, the predeclared identifier iota represents successive untyped integer constants. Its value is the index of the respective ConstSpec in that constant declaration, starting at zero. It can be used to construct a set of related constants:
+
+// const (
+// 	c0 = iota  // c0 == 0
+// 	c1 = iota  // c1 == 1
+// 	c2 = iota  // c2 == 2
+// )
+
+// const (
+// 	a = 1 << iota  // a == 1  (iota == 0)
+// 	b = 1 << iota  // b == 2  (iota == 1)
+// 	c = 3          // c == 3  (iota == 2, unused)
+// 	d = 1 << iota  // d == 8  (iota == 3)
+// )
+
+// convertion
+
+// int to float
+var i int = 42
+var f float64 = float64(i)
+
+// float to int
+var f2 float64 = 42.123
+var i2 int = int(f2)
+
+// string to int, need to import strconv
+var strC1 string = "42"
+var intC1, err = strconv.ParseInt(strC1, 10, 64) // 10 is the base, 64 is the bit size
+
+// string to float, need to import strconv
+var strC2 string = "42.123"
+var floatC2, err2 = strconv.ParseFloat(strC2, 64) // 64 is the bit size
+
+// int to string, need to import strconv
+var strC3 string = strconv.Itoa(42)
+
+// float to string, need to import strconv
+var strC4 string = strconv.FormatFloat(42.123, 'f', 6, 64) // 'f' is the format, 6 is the precision, 64 is the bit size
+
+// boolean to string, need to import strconv
+var strC5 string = strconv.FormatBool(true)
+
+// string to boolean, need to import strconv
+var boolC6, err6 = strconv.ParseBool("true")
+
+// string to byte
+var byteC7 = []byte("test")
+
+// byte to string
+var strC8 = string([]byte{116, 101, 115, 116})
+
+// rune to string
+var strC9 = string([]rune{116, 101, 115, 116})
+
+// string to rune
+var runeC10 = []rune("test")
+
 func main() {
 	test := "test" //suggar sintax
 	fmt.Printf("addres: %s\r\n", address)
@@ -62,4 +141,5 @@ func main() {
 	fmt.Printf("addres2: %s\r\n", Addres2)
 	fmt.Printf("phone2: %s\r\n", phone2)
 	fmt.Printf("test: %s\r\n", test)
+	fmt.Printf("Int convert: %d", intC1)
 }
