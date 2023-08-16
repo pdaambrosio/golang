07ac8ddbea4bd80849b3dde9dafb4f88b0cc5131
pdaambrosio <pda.ambrosio@gmail.com>
2023-08-15 22:14:05 -0300

    feat: struct car

--- interfaces2/interfaces2.go
+++ interfaces2/interfaces2.go
@@ -2,12 +2,30 @@ package main
 
 import "fmt"
 
+type Car struct {
+	manufacturer string
+	model string
+	year int
+}
+
 type People struct {
 	name string
 	age int
+	Car Car
 }
 
 func main() {
-	var p1 People = People{"zhangsan", 18}
+	var bmw Car = Car{
+		manufacturer: "bmw",
+		model: "x5",
+		year: 2020,
+	}
+
+	var p1 People = People{
+		name: "John",
+		age: 20,
+		Car: bmw,
+	}
+	
 	fmt.Println(p1)
 }
