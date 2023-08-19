3b93e1187fe04677856ed4a1ad04ea3a8afd1035
pdaambrosio <pda.ambrosio@gmail.com>
2023-08-18 23:56:24 -0300

    feat: go channels again comments

--- goChannels2/goChannels2.go
+++ goChannels2/goChannels2.go
@@ -5,31 +5,33 @@ import (
 	"time"
 )
 
-func numbers(done chan <- bool) {
+// The function numbers will send a value to the channel done
+func numbers(done chan <- bool) { // send only channel (send to channel)
 	for i := 0; i < 10; i++ {
 		time.Sleep(150 * time.Millisecond)
 		fmt.Printf("%d ", i)
 	}
-	done <- true
+	done <- true // send a value to the channel
 }
 
-func letters(done chan <- bool) {
+// The function letters will send a value to the channel done
+func letters(done chan <- bool) { // send only channel (send to channel)
 	for i := 'a'; i < 'l'; i++ {
 		time.Sleep(230 * time.Millisecond)
 		fmt.Printf("%c ", i)
 	}
-	done <- true
+	done <- true // send a value to the channel
 }
 
 func main() {
-	done1 := make(chan bool)
-	done2 := make(chan bool)
+	done1 := make(chan bool) // create a channel of type bool (send only channel)
+	done2 := make(chan bool) // create a channel of type bool (send only channel)
 
-	go numbers(done1)
-	go letters(done2)
+	go numbers(done1) // call the function numbers(done1)
+	go letters(done2) // call the function letters(done2)
 
-	<-done1
-	<-done2
+	<-done1 // receive a value from the channel done1
+	<-done2 // receive a value from the channel done2
 
 	fmt.Println("main terminated")
 }
