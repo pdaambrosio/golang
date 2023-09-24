b4e09fa9e0a6dfcc2287b766fea77b87f3c73025
pdaambrosio <pda.ambrosio@gmail.com>
2023-09-24 19:06:37 -0300

    feat: complement pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -9,6 +9,9 @@ Pointers are also useful when you want to change the value of a variable inside 
 */
 
 func main() {
+	a := 10 // this is a variable
+	b := &a // this is a pointer to a variable (memory address of a)
+
 	var firstPointer *string // this pointer isn't initialized yet
 	// firstPointer = "Mafalda" // this is a string literal, not a pointer to a string and it will throw an error
 
@@ -18,6 +21,8 @@ func main() {
 	thirdPointer := "Black Bozo" // this is a string literal
 	pointToThirdPointer := &thirdPointer // this is a pointer to a string literal (address of thirdPointer)
 	
+	fmt.Println(a, b) // 10 0xc00009e230
+	fmt.Println(a, *b) // 10 10 (desreferencing value)
 	fmt.Println(firstPointer) // <nil> (zero value) - no memory address
 	fmt.Println(secondPointer) // 0xc00009e230 (dynamic memory address)
 	fmt.Println(*secondPointer) // Mafalda (desreferencing value)
